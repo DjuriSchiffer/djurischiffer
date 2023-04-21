c65d645904fc6defdcc332dd648a8453b204f12a
Djuri Schiffer <djuri_schiffer@hotmail.com>
2023-04-21 14:41:31 +0200

    Added tailwind and Navbar

---
+++ tailwind.config.cjs
@@ -0,0 +1,46 @@
+/** @type {import('tailwindcss').Config} */
+const colors = require("tailwindcss/colors");
+
+module.exports = {
+  content: [
+    "./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}",
+    "./node_modules/flowbite/**/*.js",
+  ],
+  theme: {
+    colors: {
+      emerald: colors.emerald,
+      white: "#ffffff",
+      black: "#000000",
+      loss: "#ea3943",
+      dark: "#1e293b",
+      "gray-dark": "#1e2126",
+      gray: "#8492a6",
+      "gray-light": "#d3dce6",
+    },
+    extend: {
+      colors: {
+        blue: {
+          // primary
+          50: "#ecfdf5",
+          100: "#d1fae5",
+          200: "#a7f3d0",
+          300: "#6ee7b7",
+          400: "#34d399",
+          500: "#10b981",
+          600: "#10b981",
+          700: "#10b981",
+          800: "#10b981",
+          900: "#10b981",
+          DEFAULT: "#34d399", //500
+          on: "#ecfdf5", //50
+          dark: {
+            DEFAULT: "#34d399", //400
+            on: "#ecfdf5", //800
+          },
+        },
+      },
+    },
+  },
+  plugins: [require("flowbite/plugin")],
+  darkMode: "class",
+};
